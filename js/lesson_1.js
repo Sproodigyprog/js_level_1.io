47beb5612355a8417b17d7dd64a61cb17719ae1c
Sproodigyprog <sproodigyprog@gmail.com>
2020-07-15 21:41:25 +0400

    Create course

---
+++ js/lesson_1.js
@@ -0,0 +1,19 @@
+// First exercise
+
+const tempCelsius = 40
+
+alert('Temp Fahrenheit: ' + ((9 / 5) * tempCelsius + 32))
+
+// Second exercise
+
+let admin
+let name
+
+name = 'Василий'
+admin = name
+
+alert(admin)
+
+// Third exercise
+
+alert("1000 + '108' = 1000108 (String)")
