8980859572443caeed6608d7d8676d13722b4a7c
Sproodigyprog <sproodigyprog@gmail.com>
2020-07-21 19:37:49 +0400

    Worked on bugs (function 'power' completed)

--- js/lesson_2.js
+++ js/lesson_2.js
@@ -115,7 +115,13 @@ function power(val, pow) {
 
   if (pow === 1) return val
 
-  return val * power(val, pow-1)
+  if (pow < 0) {
+    pow = Math.abs(pow)
+    return 1 / (val * power(val, pow-1))
+  } else {
+    return val * power(val, pow-1)
+  }
+
 }
 
 alert('Exponentiation result: ' + power(val, pow))
