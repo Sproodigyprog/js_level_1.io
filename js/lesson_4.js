f00c73358fcd9a44c05f0954b0fdb3e6217b714c
Sproodigyprog <sproodigyprog@gmail.com>
2020-07-28 23:01:17 +0400

    Lesson 5 almost finished

---
+++ js/lesson_4.js
@@ -0,0 +1,185 @@
+// Exercise 1
+
+console.log('Exercise 1')
+
+// Example 1
+
+console.log('Example 1')
+
+function nToObj(n) {
+  if (n < 0) n = Math.abs(n)
+
+  let obj = {}
+  const nStr = n.toString()
+  const nStrLength = nStr.length
+
+  if(n > 9999) {
+      console.log('The number is too big!')
+      return obj
+  }
+
+  for(let i = 1; i <= nStrLength; i++) {
+    switch(-i) {
+      case -1:
+        if ((nStr[nStrLength-1]) === undefined) break
+        obj['ones'] = Number(nStr[nStrLength-1])
+      case -2:
+        if ((nStr[nStrLength-2]) === undefined) break
+        obj['tens'] = Number(nStr[nStrLength-2])
+      case -3:
+        if ((nStr[nStrLength-3]) === undefined) break
+        obj['hundreds'] = Number(nStr[nStrLength-3])
+      case -4:
+        if ((nStr[nStrLength-4]) === undefined) break
+        obj['thousands'] = Number(nStr[nStrLength-4])
+    }
+  }
+  return obj
+}
+
+console.log(nToObj(3522))
+
+// Example 2
+
+console.log('Example 2')
+
+function nToObj2(n) {
+  let obj = {}
+
+  if(n < 0 || n > 999) {
+    console.log('The number must be in the range 0 ... 999!')
+    return obj
+  }
+
+  const hundreds = Math.floor(n / 100)
+  const tens = Math.floor((n - hundreds * 100) / 10)
+  const ones = n - (hundreds * 100 + tens * 10)
+
+  obj['hundreds'] = hundreds
+  obj['tens'] = tens
+  obj['ones'] = ones
+
+  return obj
+}
+
+console.log(nToObj2(530))
+
+// Exercise 2
+
+console.log('Exercise 2')
+
+
+const sneakers = {
+  name: 'sneakers',
+  brand: 'nike',
+  article: 'n-s-389',
+  color: 'white',
+  price: 3999,
+}
+
+const shirt = {
+  name: 'shirt',
+  brand: 'columbia',
+  article: 'c-s-890',
+  color: 'grey',
+  price: 2999,
+}
+
+const socks = {
+  name: 'socks',
+  brand: 'adidas',
+  article: 'a-s-330',
+  color: 'white',
+  price: 999,
+}
+
+function fillBasket() {
+  const arr = [sneakers, shirt, socks]
+  const basket = []
+
+  while (basket.length < 5) {
+    basket.push(arr[Math.floor(Math.random() * 3)])
+  }
+  return basket
+}
+
+function countBasketPrice() {
+  const basket = fillBasket()
+  let basketSum = 0
+
+ for (const item of basket) {
+   console.log(`${item.name}: ${item.price}`)
+ }
+
+  basket.forEach(function(product) {
+    basketSum += product.price
+  })
+
+  return basketSum
+}
+
+console.log(`Sum of basket: ${countBasketPrice()}`)
+
+// Exercise 3
+
+class Product {
+  constructor (name, brand, color, price) {
+    this._name = name
+    this._brand = brand
+    this._color = color
+    this._price = price
+  }
+
+  get name() {
+    return this._name
+  }
+
+  get brand() {
+    return this._brand
+  }
+
+  get color() {
+    return this._color
+  }
+
+  get price() {
+    return this._price
+  }
+}
+
+class Card extends Product {
+  constructor(good) {
+    super()
+    this._good = good
+  }
+
+  showProductData() {
+    document.getElementsByClassName('card--name')[0].innerHTML = `Name: ${this._good._name}`
+    document.getElementsByClassName('card--brand')[0].innerHTML = `Brand: ${this._good._brand}`
+    document.getElementsByClassName('card--color')[0].innerHTML = `Color: ${this._good._color}`
+    document.getElementsByClassName('card--price')[0].innerHTML = `Price: ${this._good._price}`
+  }
+}
+
+class Basket extends Product {
+  constructor(goods) {
+    super()
+    this._goods = goods
+  }
+
+  showBasketData() {
+    this._goods.forEach(function(good, ind, arr) {
+      document.getElementsByClassName(`basket--brand-${ind}`)[0].innerHTML = `<b>${arr[ind]._brand}</b>`
+      document.getElementsByClassName(`basket--name-${ind}`)[0].innerHTML = `Name: ${arr[ind]._name}`
+      document.getElementsByClassName(`basket--price-${ind}`)[0].innerHTML = `Price: ${arr[ind]._price}`
+    })
+  }
+}
+
+const jacket = new Product('Jacket', 'Nike', 'Lightgreen', '3999')
+const gloves = new Product('Gloves', 'Columbia', 'Black', '1999')
+const card = new Card(jacket)
+const basket = new Basket([jacket, gloves])
+
+card.showProductData()
+basket.showBasketData()
--- js/lesson_5.js
+++ js/lesson_5.js
@@ -42,8 +42,102 @@ function createBoard() {
 
 createBoard()
 
-// Exercise 2
+// Exercise 2 and Exercise 3
 
+class Product {
+  constructor(name, brand, color, price) {
+    this._name = name
+    this._brand = brand
+    this._color = color
+    this._price = price
+  }
+}
 
+class productCard extends Product {
+  constructor(product) {
+    super()
+    this._product = product
+  }
 
-// Exercise 3
+  render () {
+    const catalog = document.querySelector('.catalog')
+    const target = document.createElement('div')
+    const targetName = document.createElement('p')
+    const targetBrand = document.createElement('p')
+    const targetColor = document.createElement('p')
+    const targetPrice = document.createElement('p')
+
+    target.classList.add('product-card')
+
+    targetName.innerHTML = `<b>Name</b>: ${this._product._name}`
+    targetBrand.innerHTML = `<b>Brand:</b> ${this._product._brand}`
+    targetColor.innerHTML = `<b>Color:</b> ${this._product._color}`
+    targetPrice.innerHTML = `<b>Price:</b> ${this._product._price}`
+
+    targetName.classList.add('.product-card--name')
+    targetBrand.classList.add('.product-card--brand')
+    targetColor.classList.add('.product-card--color')
+    targetPrice.classList.add('.product-card--price')
+
+    target.appendChild(targetName)
+    target.appendChild(targetBrand)
+    target.appendChild(targetColor)
+    target.appendChild(targetPrice)
+    catalog.appendChild(target)
+  }
+}
+
+class Catalog {
+  _catalog = []
+  _goodsCount = Math.floor(Math.random() * 20)
+
+  constructor(cards) {
+    this._cards = cards
+  }
+
+  fillCatalog() {
+    while(this._catalog.length <= this._goodsCount) {
+      this._catalog.push(this._cards[Math.floor(Math.random() * 4)])
+    }
+    return this._catalog
+  }
+
+  show() {
+    this._catalog.forEach(card => card.render())
+  }
+
+  get catalog() {
+    return this._catalog
+  }
+
+}
+
+class Cart extends Catalog {
+  constructor(goods) {
+    super()
+    this._goods = goods
+  }
+
+  calc() {
+    console.log(this._goods)
+  }
+}
+
+const socks = new Product('Socks', 'Nike', 'White', '999')
+const gloves = new Product('Gloves', 'Columbia', 'Blue', '1999')
+const tShirt = new Product('T-shirt', 'Adidas', 'Grey', '2999')
+const sneakers = new Product('Sneakers', 'Skechers', 'Green', '4999')
+
+const socksCard = new productCard(socks)
+const glovesCard = new productCard(gloves)
+const sneakersCard = new productCard(sneakers)
+const tShirtCard = new productCard(tShirt)
+
+const allCards = [socksCard, glovesCard, sneakersCard, tShirtCard]
+const catalog = new Catalog(allCards)
+
+const goods = catalog.fillCatalog()
+catalog.show()
+
+const cart = new Cart(goods)
+cart.calc()
