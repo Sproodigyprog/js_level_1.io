1b2cd35d5cd6f4ae1c44310f059d44873c617afb
Sproodigyprog <sproodigyprog@gmail.com>
2020-08-05 23:50:37 +0400

    Lesson 7 finished

--- js/lesson_7.js
+++ js/lesson_7.js
@@ -136,14 +136,12 @@ class Card {
         if (n >= 0) {
           if ((i + 1) === slides.length) slides[0].classList.add('modal--block_active')
           else slides[i + 1].classList.add('modal--block_active')
-
           break
         }
 
         if (n < 0) {
           if ((i - 1) < 0) slides[slides.length-1].classList.add('modal--block_active')
           else slides[i - 1].classList.add('modal--block_active')
-
           break
         }
       }
@@ -202,41 +200,54 @@ class Card {
 
   _renderCatalog() {
     const catalog = document.querySelector('.catalog')
-    this._card = document.createElement('div')
-    this._name = document.createElement('p')
-    this._brand = document.createElement('p')
-    this._color = document.createElement('p')
-    this._price = document.createElement('p')
 
-    this._name.innerHTML = `<b>Name: </b>${this._product._name}`
-    this._brand.innerHTML = `<b>Brand: </b>${this._product._brand}`
-    this._color.innerHTML = `<b>Color: </b>${this._product._color}`
-    this._price.innerHTML = `<b>Price: </b>${this._product._price}`
+    if (catalog) {
+      this._card = document.createElement('div')
+      this._name = document.createElement('p')
+      this._brand = document.createElement('p')
+      this._color = document.createElement('p')
+      this._price = document.createElement('p')
 
-    this._card.classList.add('card')
-    this._name.classList.add('card--data', 'card_name')
-    this._brand.classList.add('card--data', 'card_brand')
-    this._color.classList.add('card--data', 'card_color')
-    this._price.classList.add('card--data', 'card_price')
+      this._name.innerHTML = `<b>Name: </b>${this._product._name}`
+      this._brand.innerHTML = `<b>Brand: </b>${this._product._brand}`
+      this._color.innerHTML = `<b>Color: </b>${this._product._color}`
+      this._price.innerHTML = `<b>Price: </b>${this._product._price}`
 
-    this._card.appendChild(this._addImg(this._product._name))
-    this._card.appendChild(this._name)
-    this._card.appendChild(this._brand)
-    this._card.appendChild(this._color)
-    this._card.appendChild(this._price)
-    this._card.appendChild(this._createBtn())
-    catalog.appendChild(this._card)
+      this._card.classList.add('card')
+      this._name.classList.add('card--data', 'card_name')
+      this._brand.classList.add('card--data', 'card_brand')
+      this._color.classList.add('card--data', 'card_color')
+      this._price.classList.add('card--data', 'card_price')
+
+      this._card.appendChild(this._addImg(this._product._name))
+      this._card.appendChild(this._name)
+      this._card.appendChild(this._brand)
+      this._card.appendChild(this._color)
+      this._card.appendChild(this._price)
+      this._card.appendChild(this._createAddToCartBtn())
+      this._card.appendChild(this._createToCartBtn())
+      catalog.appendChild(this._card)
+    }
   }
 
-  _createBtn() {
+  _createAddToCartBtn() {
     const btn = document.createElement('button')
 
     btn.classList.add('card--btn')
     btn.innerHTML = 'Add to cart'
-
     btn.addEventListener('click', this.purchase.bind(this))
 
     return btn
+  }
+
+  _createToCartBtn() {
+    const link = document.createElement('a')
+
+    link.href = 'cart.html'
+    link.classList.add('card--btn', 'card--to-cart-btn')
+    link.innerHTML = 'Go to cart'
+
+    return link
   }
 
   purchase() {
@@ -245,15 +256,43 @@ class Card {
       cart.addToCart({
         name: this._product._name,
         price: +this._product._price,
+        sum: +this._product._price,
+        amount: 1,
       })
-      cart._renderCart()
-    } else {
+    }
+
+    if (!cart.getCart.length) {
       cart.addToCart({
         name: this._product._name,
         price: +this._product._price,
+        sum: +this._product._price,
+        amount: 1,
       })
-      cart._renderCart()
+    } else {
+      const matches = cart.getCart.map((product, ind, arr) => {
+        return product.name
+      })
+
+      const cartLocal = cart.getCart
+
+      for (let i = 0; i < cartLocal.length; i++) {
+        if (matches.indexOf(this._product._name) === -1) {
+          cart.addToCart({
+            name: this._product._name,
+            price: +this._product._price,
+            sum: +this._product._price,
+            amount: 1,
+          })
+          break
+        } else if (cartLocal[i].name === this._product._name) {
+          cartLocal[i].amount++
+          cartLocal[i].sum = cartLocal[i].amount * cartLocal[i].price
+          break
+        }
+      }
     }
+    document.cookie = `cart=${JSON.stringify(cart.getCart)}; sameSite=strict; Secure`
+    cart._renderCart()
   }
 }
 
@@ -270,6 +309,14 @@ class Cart {
     this._cart.push(data)
   }
 
+  _animateElement(elem) {
+    elem.animate([
+  // keyframes
+      { left: '0', opacity: '1' },
+      { left: '100px', opacity: '0' },
+    ], 200)
+  }
+
   _clearRenderedCart() {
     const cartChildren = document.querySelector('.cart').children
     let childrenLength = cartChildren.length
@@ -283,6 +330,88 @@ class Cart {
   _clearCart() {
     this._cart = []
     this._renderCart()
+  }
+
+  _createCollapseCartBtn() {
+    const btn = document.createElement('a')
+
+    btn.classList.add('cart--collapse-btn')
+    btn.href = '#'
+    btn.innerHTML = '\u02c5'
+    btn.addEventListener('click', this._collapseCart.bind(this))
+
+    return btn
+  }
+
+  _collapseCart() {
+    event.preventDefault()
+    document.querySelectorAll('.cart--item-wrp').forEach(item => {
+      item.classList.toggle('cart_collapse')
+    })
+
+    const arrow = document.querySelector('.cart--collapse-btn')
+    arrow.innerHTML === '\u02c4' ? arrow.innerHTML = '\u02c5' : arrow.innerHTML = '\u02c4'
+  }
+
+  _createAddProductAmountBtn(productInd) {
+    const btn = document.createElement('button')
+
+    btn.classList.add('cart--btn', 'cart-btn_item')
+    btn.innerHTML = '+'
+    btn.addEventListener('click', () => {
+      this._addProductAmount(productInd)
+    })
+    return btn
+  }
+
+  _addProductAmount(productInd) {
+    const elem = document.querySelector('.card--total-data--amount')
+    this._cart[productInd].amount++
+    this._animateElement(elem)
+    setTimeout(this._renderCart.bind(this), 300)
+    this._cart[productInd].sum = this._cart[productInd].amount * this._cart[productInd].price
+  }
+
+  _createReduceProductAmountBtn(productInd) {
+    const btn = document.createElement('button')
+
+    btn.classList.add('cart--btn', 'cart-btn_item')
+    btn.innerHTML = '-'
+    btn.addEventListener('click', () => {
+      this._reduceProductAmount(productInd)
+    })
+    return btn
+  }
+
+  _reduceProductAmount(productInd) {
+    const product = this._cart[productInd]
+    const elem = document.querySelector('.card--total-data--amount')
+
+    if (product.amount > 0) {
+      product.amount--
+      this._animateElement(elem)
+      setTimeout(this._renderCart.bind(this), 300)
+      product.sum = product.amount * product.price
+    }
+  }
+
+  _createRmProductBtn(productInd) {
+    const btn = document.createElement('button')
+
+    btn.classList.add('cart--btn', 'cart-btn_item')
+    btn.innerHTML = 'x'
+    btn.addEventListener('click', () => {
+      this._rmProduct(productInd)
+    })
+    return btn
+  }
+
+  _rmProduct(productInd) {
+    const elem = document.querySelector('.card--total-data--amount')
+    event.target.parentElement.remove()
+    this._cart.splice(productInd, 1)
+    this._animateElement(elem)
+    setTimeout(this._renderCart.bind(this), 300)
   }
 
   _createClearCartBtn() {
@@ -305,15 +434,26 @@ class Cart {
 
   _createCartTotalData() {
     const cartTotalData = document.createElement('p')
+    const cartTotalDataAmount = document.createElement('span')
+    const cartTotalDataSum = document.createElement('span')
     let cartTotalAmount = 0,
-        cartTotalPrice = 0
+        cartTotalSum = 0
 
     this._cart.forEach(item => {
-      cartTotalAmount++
-      cartTotalPrice += item.price
+      cartTotalAmount+=item.amount
+      cartTotalSum += item.sum
     })
 
-    cartTotalData.innerHTML = `<b>Total amount:</b> ${cartTotalAmount}<br><b>Total price:</b> ${cartTotalPrice}`
+    cartTotalData.classList.add('cart--total-data')
+    cartTotalDataAmount.classList.add('card--total-data--amount')
+    cartTotalDataSum.classList.add('card--total-data--sum')
+
+    cartTotalDataAmount.innerHTML = `${cartTotalAmount}`
+    cartTotalDataSum.innerHTML = `${cartTotalSum}`
+    cartTotalData.innerHTML = `<b>Total amount: </b>`
+    cartTotalData.appendChild(cartTotalDataAmount)
+    cartTotalData.innerHTML += `<br><b>Total sum: </b>`
+    cartTotalData.appendChild(cartTotalDataSum)
 
     return cartTotalData
   }
@@ -322,21 +462,37 @@ class Cart {
     this._clearRenderedCart()
 
     const cart = document.querySelector('.cart')
-    const wrp = document.createElement('div')
+    const totalDataWrp = document.createElement('div')
 
-    wrp.classList.add('cart--wrp')
+    totalDataWrp.classList.add('cart--total-data-wrp')
+    totalDataWrp.id = 'cart'
     cart.classList.add('cart_rendered')
+    cart.appendChild(this._createCartDelimiter())
 
     this._cart.forEach((item, ind) => {
       const cartItem = document.createElement('p')
-      cartItem.innerHTML = `${ind+1} — <b>Name:</b> ${item.name} <b>Price:</b> ${item.price}`
-      cart.appendChild(cartItem)
+      const cartItemWrp = document.createElement('div')
+
+      cartItemWrp.classList.add('cart--item-wrp')
+      cartItem.innerHTML = `${ind+1} __________________ <br><b>Name:</b> ${item.name} <b>Price:</b> ${item.price}<br><b>Sum:</b> ${item.sum} <b>Amount:</b> ${item.amount}`
+      cartItem.classList.add('cart--item')
+
+      cartItemWrp.appendChild(cartItem)
+      cartItemWrp.appendChild(this._createAddProductAmountBtn(ind))
+      cartItemWrp.appendChild(this._createReduceProductAmountBtn(ind))
+      cartItemWrp.appendChild(this._createRmProductBtn(ind))
+      cart.appendChild(cartItemWrp)
     })
 
     cart.appendChild(this._createCartDelimiter())
-    wrp.appendChild(this._createCartTotalData())
-    wrp.appendChild(this._createClearCartBtn())
-    cart.appendChild(wrp)
+    totalDataWrp.appendChild(this._createCartTotalData())
+    totalDataWrp.appendChild(this._createClearCartBtn())
+    cart.appendChild(this._createCollapseCartBtn())
+    cart.appendChild(totalDataWrp)
+  }
+
+  get getCart() {
+    return this._cart
   }
 }
 
